011d6801fdad5e63ce3ec447576b9fa39111e7d9
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-06-01 20:21:09 +0530

    Time: 12 ms (13.59%), Space: 11.1 MB (71.76%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,23 +4,24 @@ public:
         int s=0, e=nums.size()-1;
         while(s<=e)
         {
-            int mid=(s+e)/2;
-            if(nums[mid]==target) 
-                return mid;
-            else if(nums[s]<=nums[mid]){
-                if(nums[s]<=target and nums[mid]>=target) 
+            int mid=s+((e-s)/2);
+            //cout<<mid;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>=nums[s])
+            {
+                if(nums[s]<=target and target <= nums[mid]) 
                     e=mid-1;
-                else 
+                else
                     s=mid+1;
             }
             else
             {
-                if(nums[mid]<=target and nums[e]>=target) 
+                if(nums[mid] <= target and target <= nums[e]) 
                     s=mid+1;
-                else 
+                else
                     e=mid-1;
-            }            
+            }
         }
         return -1;
-    }  
+    }
 };
