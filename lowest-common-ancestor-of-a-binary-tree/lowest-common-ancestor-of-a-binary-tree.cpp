3ccefa4259a05edd0cf59a22ca89def3c42a6657
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-07-22 00:42:05 +0530

    Time: 16 ms (70.66%), Space: 14.2 MB (84.63%) - LeetHub

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,53 +9,26 @@
  */
 class Solution {
 public:
-    bool node2root(TreeNode *root, int target, vector<TreeNode*> &ans)
+    TreeNode *LCANode = nullptr;
+    bool LCA(TreeNode *root, TreeNode *p, TreeNode *q)
     {
-        if(root==NULL) return false;
-        if(root->val == target)
-        {
-            ans.push_back(root);
-            return true;
-        }
-        bool res = node2root(root->left, target, ans) || node2root(root->right, target, ans);
-        if(res) ans.push_back(root);
-        return res;
+        if (root == nullptr)
+            return false;
+        bool selfPresent = (root == p || root == q);
+
+        bool leftPresent = LCA(root->left, p, q);
+        bool rightPresent = LCA(root->right, p, q);
+        
+        if ((leftPresent && rightPresent) || (leftPresent && selfPresent) || (rightPresent && selfPresent))
+            LCANode = root;
+
+        return leftPresent || rightPresent || selfPresent;
     }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> ansP;
-        vector<TreeNode*> ansQ;
-        bool P = node2root(root, p->val, ansP);
-        bool Q = node2root(root, q->val, ansQ);
-        int pi = ansP.size()-1, qi = ansQ.size()-1;
-        TreeNode* LCA = NULL;
-        while(pi>=0 && qi>=0)
-        {
-            if(ansP[pi] != ansQ[qi]) break;
-            LCA = ansP[pi];
-            pi--;
-            qi--;            
-        }
-        return LCA;
+
+    TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+    {
+        LCA(root, p, q);
+        return LCANode;
     }
+
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
