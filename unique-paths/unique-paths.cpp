fa2b2693cd47c9f28212dc5b8e51cbebd839d8f2
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-11-11 22:22:24 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (23.07%) - LeetHub

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int uniquePaths(int sr, int sc, int er, int ec, vector<vector<int>> &dp, vector<vector<int>> dir) {
-        if (er == sr && ec == sc) {
+    int uni(int sr, int sc, int er, int ec, vector<vector<int>> &dp, vector<vector<int>> &dir)
+    {
+        if(sr==er && sc==ec)
             return dp[sr][sc] = 1;
-        }
-
-        if (dp[sr][sc] != 0)
+        if(dp[sr][sc]!=-1)
             return dp[sr][sc];
-
-        int count = 0;
-        for (vector<int>d : dir) {
-            int r = sr + d[0], c = sc + d[1];
-            if (r >= 0 && c >= 0 && r < dp.size() && c < dp[0].size()) {
-                count += uniquePaths(r, c, er, ec, dp, dir);
-            }
+        int count=0;
+        for(int d=0; d<dir.size();d++)
+        {
+            int r = sr + dir[d][0], c= sc + dir[d][1];
+            if(r>=0 && c>=0 && r<dp.size() && c<dp[0].size())
+                count+=uni(r,c,er,ec,dp, dir);
         }
-
         return dp[sr][sc] = count;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dir{{0,1},{1,0}};
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        return uniquePaths(0,0,m-1,n-1,dp,dir);
+        vector<vector<int>> dp(m, vector<int>(n,-1));
+        vector<vector<int>> dir = {{0,1},{1,0}};
+        return uni(0,0,m-1,n-1,dp,dir);
     }
 };
