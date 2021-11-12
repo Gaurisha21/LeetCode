4d48ee69be86479069ab5b3c3dc507780d2f96b0
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-11-12 11:23:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (13.32%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int climbStairs_dp(int N, vector<int> &dp)
-{
-
-    for (int n = 0; n <= N; n++)
+    int step(int n, vector<int> &dp)
     {
-        if (n <= 1)
-        {
-            dp[n] = 1;
-            continue;
-        }
-
-        dp[n] = dp[n - 1] + dp[n - 2]; //climbStairs_memo(n - 1, dp) + climbStairs_memo(n - 2, dp);
+        if(n<=1)
+            return dp[n]=1;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n] = step(n-1,dp)+step(n-2,dp);
     }
-
-    return dp[N];
-}
-
-int climbStairs(int n)
-{
-    vector<int> dp(n + 1, 0);
-    return climbStairs_dp(n, dp);
-}
-
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return step(n,dp);
+    }
 };
