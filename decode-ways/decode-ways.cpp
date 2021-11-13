d21e7862cd182b7a6c8114ab96cd2016677bb04a
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-11-13 17:24:18 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (14.56%) - LeetHub

--- decode-ways/decode-ways.cpp
+++ decode-ways/decode-ways.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int numDecodings(string s, int idx, vector<int> &dp) {
-        if(idx == s.length()) return dp[idx] = 1;
-        if(dp[idx]!=-1) return dp[idx];
-        char ch1 = s[idx];
-        if (ch1 == '0')
+    int nd(int i, string s, vector<int> &dp)
+    {
+        int n=s.length();
+        if(i==n)
+            return dp[i]=1;
+        if(dp[i]!=-1)
+            return dp[i];
+        char ch1 = s[i];
+        if(ch1=='0')
             return 0;
-        int count = 0;
-        count+=numDecodings(s, idx+1, dp);
-        if(idx < s.length()-1)
+        int count=0;
+        count+=nd(i+1,s,dp);
+        if(i<n-1)
         {
-            char ch2 = s[idx+1];
+            char ch2 = s[i+1];
             int num = (ch1-'0')*10 + (ch2-'0');
             if(num<=26)
-                count+=numDecodings(s, idx+2, dp);
+                count+=nd(i+2,s,dp);
         }
-        return dp[idx] = count;
+        return dp[i] = count;
     }
     int numDecodings(string s) {
-        int n = s.length();
-        vector<int> dp(n+1,-1);
-        return numDecodings(s,0,dp);
+        int n=s.length();
+        vector<int>dp(n+1,-1);
+        return nd(0,s,dp);
     }
 };
