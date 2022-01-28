ad0259e21197c9a342672b3893f1c872332c7678
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-01-28 20:33:37 +0530

    Time: 303 ms (5.02%), Space: 84.7 MB (8.09%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int insertPos(vector<int> arr, int x)
+    {
+        int n=arr.size(), s=0, e=n-1, mid;
+        while(s<=e)
+        {
+            mid = (s+(e-s)/2);
+            if(arr[mid]>x) e=mid-1;
+            else s=mid+1;
+        }
+        return s-1>=0 and arr[s-1]==x ? s-1 : s;
+    }
+    bool increasingTriplet(vector<int>& arr) {
+        int n=arr.size();
+        if(n<=2) return false;
+        if(n<=1) return n;
+        vector<int> ans;
+        // ans.push_back(arr[0]);
+        for(int ele : arr)
+        {
+            int ps = insertPos(ans,ele);
+            if(ps==ans.size()) 
+            {
+                ans.push_back(ele);
+                if(ans.size()==3) return true;
+            }
+            else ans[ps] = ele;
+        }
+        return ans.size()==3;
+    }
+};
