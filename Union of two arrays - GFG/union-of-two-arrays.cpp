eed1f7c781a814ecd54f9e8c51a30ec2272cf117
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-02-07 00:25:33 +0530

    Added solution - LeetHub

---
+++ Union of two arrays - GFG/union-of-two-arrays.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template in C++
+
+class Solution{
+    public:
+    //Function to return the count of number of elements in union of two arrays.
+    int doUnion(int arr1[], int n, int arr2[], int m)  {
+        unordered_map<int,int> map;
+        int k=0;
+        for(int i=0; i<n; i++)
+            map[arr1[i]]++;
+        for(int i=0; i<m; i++)
+            map[arr2[i]]++;
+        for(auto i:map)
+        {
+            k++;
+        }
+        return k;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int n, m;
+	    cin >> n >> m;
+	    int a[n], b[m];
+	   
+	    for(int i = 0;i<n;i++)
+	       cin >> a[i];
+	       
+	    for(int i = 0;i<m;i++)
+	       cin >> b[i];
+	    Solution ob;
+	    cout << ob.doUnion(a, n, b, m) << endl;
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
