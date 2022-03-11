71d14f01c2e34c732a8c617f46c8daaafd875957
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-03-11 16:33:16 +0530

    Time: 11 ms (55.42%), Space: 11.1 MB (59.64%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -12,31 +12,29 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(head==NULL || head->next==NULL) return head;
-        ListNode *p=new ListNode(-1), *q=p, *temp=head;
+        ListNode *temp=head, *p=new ListNode(-1), *curr=p;
         p->next=head;
         while(temp!=NULL and temp->next!=NULL)
         {
-            // cout<<q->val<<" ";
-            if(temp->val==temp->next->val )
+            if(temp->next->val==temp->val)
             {
-                while(temp!=NULL and temp->next!=NULL and temp->val==temp->next->val)
+                while(temp->next!=NULL and temp!=NULL and temp->next->val==temp->val)
                     temp=temp->next;
-                if(temp!=NULL)
+                if(temp->next==NULL)
                 {
-                    temp=temp->next;
-                    q->next=temp;
-                }
-                else 
-                {
-                    q->next=NULL;
+                    curr->next=NULL;
                     break;
                 }
-                
+                else
+                {
+                    temp=temp->next;
+                    curr->next=temp;
+                }
             }
             if(temp!=NULL and temp->next!=NULL and temp->val!=temp->next->val)
             {
-                q->next=temp;
-                q=temp;
+                curr->next=temp;
+                curr=temp;
                 temp=temp->next;
             }
         }
