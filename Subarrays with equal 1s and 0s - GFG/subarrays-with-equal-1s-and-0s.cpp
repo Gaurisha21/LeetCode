beb5f7f73c3a13c713b20301262ed89d066bcdf4
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-03-13 13:29:11 +0530

    Added solution - LeetHub

--- Subarrays with equal 1s and 0s - GFG/subarrays-with-equal-1s-and-0s.cpp
+++ Subarrays with equal 1s and 0s - GFG/subarrays-with-equal-1s-and-0s.cpp
@@ -4,34 +4,26 @@ using namespace std;
 
 
  // } Driver Code Ends
+
 class Solution{
   public:
     //Function to count subarrays with 1s and 0s.
     long long int countSubarrWithEqualZeroAndOne(int arr[], int n)
     {
-        unordered_map<int,int> rem;
-        rem[0]=1;
-        long long int e=0, ans=0, sum=0;
-        for(long long int i=0; i<n; i++)
+        for(int i=0; i<n; i++)
         {
-            if(arr[i]==0) arr[i]=-1;
+            if(arr[i]==0)
+                arr[i]=-1;
         }
-        // for(long long int i=0; i<n; i++) cout<<arr[i]<<" ";
-        // cout<<endl;
+        int e=0,sum=0,ans=0;
+        unordered_map<long long, int> map;
+        map[0]=1;
         while(e<n)
         {
-            int val = arr[e++];
-            sum+=val;
-            // cout<<"sum = "<<sum<<" ";
-            auto it1 = rem.find(sum);
-            if (it1 != rem.end())
-            {
-                ans=ans+rem[sum];
-            }
-            else
-                ans+=0;
-            rem[sum]++;
-            // cout<<"ans = "<<ans<<" ";
+            sum+=arr[e];
+            ans+=map[sum];
+            map[sum]++;
+            e++;
         }
         return ans;
     }
