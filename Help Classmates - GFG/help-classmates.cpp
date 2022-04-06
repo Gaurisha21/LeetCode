443feb651bf917a36813efdec1b65cff32a2a015
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-04-06 14:34:51 +0530

    Added solution - LeetHub

---
+++ Help Classmates - GFG/help-classmates.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    
+    public:
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        vector<int> narr(n,-1);
+        stack<int> st;
+        st.push(-1);
+        for(int i=0; i<n; i++)
+        {
+            while(st.top()!=-1 and arr[i]<arr[st.top()])
+            {
+                narr[st.top()]=arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return narr;
+    } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0; 
+}
+
+  // } Driver Code Ends
