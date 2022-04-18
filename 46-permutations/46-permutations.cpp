cdfd53a52cdaa731325de590d40f870f63a6a475
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-04-18 22:20:27 +0530

    Time: 4 ms (65.58%), Space: 8.9 MB (8.43%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    int perm(vector<int> nums, int idx, vector<vector<int>> &ans, vector<int> &arr)
+    int perm(vector<int> nums, vector<vector<int>> &ans, vector<int> &smallAns, int idx)
     {
-        int n=nums.size();
-        if(idx==n)
+        if(idx==nums.size())
         {
-            ans.push_back(arr);
+            ans.push_back(smallAns);
             return 1;
         }
         int count=0;
-        for(int i=0; i<n; i++)
+        for(int i=0; i<nums.size(); i++)
         {
             if(nums[i]!=-11)
             {
-                vector<int> t=arr;
                 int temp=nums[i];
-                nums[i] = -11;
-                arr.push_back(temp);
-                count+=perm(nums,idx+1,ans,arr);
-                arr=t;
-                nums[i] = temp;
+                nums[i]=-11;
+                vector<int> tem = smallAns;
+                tem.push_back(temp);
+                count+=perm(nums,ans,tem,idx+1);
+                nums[i]=temp;
             }
         }
         return count;
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> arr;
-        int s=perm(nums,0,ans,arr);
-        // cout<<s;
+        vector<int> smallAns;
+        int s=perm(nums,ans,smallAns,0);
         return ans;
     }
 };
