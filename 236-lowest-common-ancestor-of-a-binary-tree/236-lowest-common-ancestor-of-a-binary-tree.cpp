90bb730b3be366cdba321a97c883dd04e96e5b76
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-04-27 11:55:23 +0530

    Time: 31 ms (18.58%), Space: 21.3 MB (5.41%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode *> nodeToRoot(TreeNode *root, TreeNode *target)//, vector<int> &path)
+    {
+        if(root==NULL)
+            return {};
+        if(root==target)
+            return {root};
+        vector<TreeNode *> l, r;
+        l=nodeToRoot(root->left,target);
+        if(l.size()!=0)
+        {
+            l.push_back(root);
+            return l;
+        }
+        r=nodeToRoot(root->right,target);
+        if(r.size()!=0)
+        {
+            r.push_back(root);
+            return r;
+        }
+        return {};
+    }
+    void reverse(vector<TreeNode *> &path)
+    {
+        int i=0, j=path.size()-1;
+        while(i<=j)
+            swap(path[i++],path[j--]);
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        vector<TreeNode *> ppath, qpath;
+        ppath = nodeToRoot(root, p);
+        qpath = nodeToRoot(root,q);
+        reverse(ppath);
+        reverse(qpath);
+        int i=0, j=0;
+        while(i<ppath.size() and j<qpath.size())
+        {
+            if(ppath[i]!=qpath[j])
+                break;
+            i++;
+            j++;
+        }
+        return ppath[i-1];
+    }
+};
