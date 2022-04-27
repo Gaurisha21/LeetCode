1245b7726bf8511bb87f3904c2a74fad7e00b3c1
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-04-27 16:30:24 +0530

    Time: 35 ms (66.20%), Space: 23.3 MB (12.96%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,47 +10,20 @@
 
 class Solution {
 public:
-    vector<TreeNode *> nodeToRoot(TreeNode *root, TreeNode *target)//, vector<int> &path)
-    {
-        if(root==NULL)
-            return {};
-        if(root==target)
-            return {root};
-        vector<TreeNode *> l, r;
-        l=nodeToRoot(root->left,target);
-        if(l.size()!=0)
-        {
-            l.push_back(root);
-            return l;
-        }
-        r=nodeToRoot(root->right,target);
-        if(r.size()!=0)
-        {
-            r.push_back(root);
-            return r;
-        }
-        return {};
-    }
-    void reverse(vector<TreeNode *> &path)
-    {
-        int i=0, j=path.size()-1;
-        while(i<=j)
-            swap(path[i++],path[j--]);
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode *> ppath, qpath;
-        ppath = nodeToRoot(root, p);
-        qpath = nodeToRoot(root,q);
-        reverse(ppath);
-        reverse(qpath);
-        int i=0, j=0;
-        while(i<ppath.size() and j<qpath.size())
+        TreeNode *curr=root, *LCA=NULL;
+        while(curr!=NULL)
         {
-            if(ppath[i]!=qpath[j])
+            if(curr->val<p->val and curr->val<q->val)
+                curr=curr->right;
+            else if(curr->val>p->val and curr->val>q->val)
+                curr=curr->left;
+            else 
+            {
+                LCA=curr;
                 break;
-            i++;
-            j++;
+            }
         }
-        return ppath[i-1];
+        return LCA;
     }
 };
