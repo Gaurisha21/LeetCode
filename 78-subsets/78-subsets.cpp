e606eba6b92b7f5948fc7cc8c0537713631f81f8
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-05-27 20:54:07 +0530

    Time: 7 ms (26.90%), Space: 16.3 MB (14.29%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int subset(int idx, vector<int>& nums, vector<vector<int>> &ans, vector<int> &arr)
+    int subset(int idx, vector<int> nums, vector<vector<int>> &ans, vector<int> &smallAns)
     {
         if(idx==nums.size())
         {
-            ans.push_back(arr);
+            ans.push_back(smallAns);
             return 1;
         }
-        int count=0, t = nums[idx];
-        vector<int> ARR = arr;
-        ARR.push_back(t);
-        count+=subset(idx+1,nums,ans,ARR);
-        count+=subset(idx+1,nums,ans,arr);
+        int count=0;
+        vector<int> temp=smallAns;
+        temp.push_back(nums[idx]);
+        count+=subset(idx+1,nums,ans,temp);
+        count+=subset(idx+1,nums,ans,smallAns);
         return count;
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> smallAns;
         vector<vector<int>> ans;
-        vector<int> arr;
-        int a = subset(0,nums,ans,arr);
+        int a = subset(0,nums,ans, smallAns);
         return ans;
     }
 };
