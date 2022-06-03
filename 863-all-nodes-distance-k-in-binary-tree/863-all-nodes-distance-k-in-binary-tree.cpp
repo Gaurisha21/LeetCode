7143c3e4c514624b7577ebf91bb6b0d9bf3bcd20
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-06-03 21:27:59 +0530

    Time: 7 ms (72.56%), Space: 12.4 MB (84.56%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,61 +9,44 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> nodeToRootPath(TreeNode* root, TreeNode* target)
+    void kdown(TreeNode* node, TreeNode* block, int k, vector<int>&ans)
     {
-        if(root==NULL) return {};
-        if(root==target)
-            return {root};
-        vector<TreeNode*> l = nodeToRootPath(root->left, target);
-        if(l.size()!=0)
-        {
-            l.push_back(root);
-            return l;
-        }
-        vector<TreeNode*> r = nodeToRootPath(root->right, target);
-        if(r.size()!=0)
-        {
-            r.push_back(root);
-            return r;
-        }
-        return {};    
-    }
-    void nodesK(TreeNode* root, int k, vector<int> &ans)
-    {
-        if(root==NULL)
+        if(node==NULL || k<0 || node==block)
             return;
         if(k==0)
         {
-            ans.push_back(root->val);
+            ans.push_back(node->val);
             return;
         }
-        if(root->left!=NULL and root->left->val>=0)
-            nodesK(root->left,k-1,ans);
-        if(root->right!=NULL and root->right->val>=0)
-            nodesK(root->right,k-1,ans);
+        kdown(node->left, block, k-1, ans);
+        kdown(node->right, block, k-1, ans);
+    }
+    int distanceK(TreeNode* root, TreeNode* target, int k, vector<int>&ans)
+    {
+        if(root==NULL)
+            return -1;
+        if(root==target)
+        {
+            kdown(root,NULL,k,ans);
+            return 1;
+        }
+        int ld = distanceK(root->left,target,k,ans);
+        if(ld!=-1)
+        {
+            kdown(root,root->left,k-ld,ans);
+            return ld+1;
+        }
+        int rd = distanceK(root->right,target,k,ans);
+        if(rd!=-1)
+        {
+            kdown(root,root->right,k-rd,ans);
+            return rd+1;
+        }
+        return -1;
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<TreeNode*> path = nodeToRootPath(root,target);
-        int n=path.size();
-        vector<int> ans;
-        for(int i=0; i<n; i++)
-        {
-            int K=k-i;
-            if(i!=0)
-                path[i-1]->val=-path[i-1]->val;
-            nodesK(path[i],K,ans);
-            if(i!=0)
-                path[i-1]->val=-path[i-1]->val;
-        }
+        vector<int>ans;
+        int x = distanceK(root, target, k, ans);
         return ans;
     }
 };
-
-
-
-
-
-
-
-
-
