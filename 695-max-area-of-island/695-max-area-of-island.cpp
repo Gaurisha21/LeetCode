1657e4421f9d8045a396142ddea424389f3100ed
Gaurisha R Srivastava <68791455+Gaurisha21@users.noreply.github.com>
2022-07-17 18:50:37 +0530

    Time: 68 ms (6.44%), Space: 25.4 MB (19.17%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
-    int gcc(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dir)
+    
+    vector<int> par, size;
+    
+    int findParent(int u)
     {
-        int n=grid.size(), m=grid[0].size(), size = 0;
-        grid[i][j]=0;
-        for(int d=0; d<dir.size(); d++)
-        {
-            int r=i+dir[d][0];
-            int c=j+dir[d][1];
-            if(r>=0 and r<n and c>=0 and c<m and grid[r][c]==1){
-                size += gcc(grid,r,c,dir);
-            }
-        }
-        return size+1;
+        return par[u]==u ? u : (par[u]=findParent(par[u]));
     }
+        
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        vector<vector<int>> dir = {{0,1},{0,-1},{1,0},{-1,0}};
-        int comp=0;
-        int n=grid.size(), m=grid[0].size(), area = 0, maxArea = 0;
-        for(int i=0; i<n; i++)
+        int n=grid.size(), m=grid[0].size(), maxAns = 0;
+        for(int i=0; i<n*m; i++)
+        {
+            par.push_back(i);
+            size.push_back(1);
+        }
+        
+        vector<vector<int>> dir{{0,1},{0,-1},{1,0},{-1,0}};
+        
+        for(int i=0; i<n ;i++)
         {
             for(int j=0; j<m; j++)
             {
                 if(grid[i][j]==1)
                 {
-                    comp++;
-                    area = gcc(grid,i,j,dir);
-                    maxArea = max(area,maxArea);
+                    int p1 = findParent(i*m+j);
+                    for(int d=0;d<dir.size(); d++)
+                    {
+                        int r=i+dir[d][0];
+                        int c=j+dir[d][1];
+                        if(r>=0 and r<n and c>=0 and c<m and grid[r][c]==1)
+                        {
+                            int p2 = findParent(r*m+c);
+                            if(p1!=p2)
+                            {
+                                par[p2]=p1;
+                                size[p1]+=size[p2];
+                            }
+                        }
+                    }
+                    maxAns = max(maxAns,size[p1]);
                 }
             }
         }
-        return maxArea;
+        return maxAns;
     }
 };
